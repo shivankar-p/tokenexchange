ea1188a64ee17d20751f911b8ca9f92f6c8487ba
shivankar <shivankarpilligundla@gmail.com>
2022-02-19 15:27:34 +0530

    backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DAppProvider, ChainId} from "@usedapp/core"
+import { Header } from "./Components/Header"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DAppProvider config={{
+        supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
+      }}>
+        <Header />
+        <div>HI</div>
+      </DAppProvider>
     </div>
   );
 }
